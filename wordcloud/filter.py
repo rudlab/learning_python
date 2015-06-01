5328153ff2e6eca0466c4d1833c447c037973c82
Baldur Eiríksson <rudlab@gmail.com>
2015-06-01 15:50:32 +0000

    Skrifar í skrá
    Ný bannorð og betri útskrift

--- wordcloud/filter.py
+++ wordcloud/filter.py
@@ -9,11 +9,10 @@ from collections import Counter
 # upphrópanir
 # nafnháttarmerki
 
-badWords = [' ','að','af','aldrei','alltaf','annaðhvort','auk','á','áður','án','ásamt','ávallt','bráðum','bæði','eða','ef','eftir','eigi','ekki','ellegar','en','enda','fram','frá','fyrir','gegn','gegnt','gegnum','handa','heim','heldur','hér','hérna','hingað','hjá','hó','hvar','hve','hvenær','hvernig','hversu','hvert','hví','hvorki','hvort','hæ','inn','í','jú','kringum','lengi','með','meðal','meðan','meðfram','mé','milli','nei','nema','né','niður','nú','og','ó','samkvæmt','sem','stundum','til','um','undan','undir','upp','uss','ú','úr','út','vegna','við','víða','yfir','ýmist','þar','þarna','þá','þegar','æ','ætíð'];
+badWords = ['er','að','af','aldrei','alltaf','annaðhvort','auk','á','áður','án','ásamt','ávallt','bráðum','bæði','eða','ef','eftir','eigi','ekki','ellegar','en','enda','fram','frá','fyrir','gegn','gegnt','gegnum','handa','heim','heldur','hér','hérna','hingað','hjá','hó','hvar','hve','hvenær','hvernig','hversu','hvert','hví','hvorki','hvort','hæ','inn','í','jú','kringum','lengi','með','meðal','meðan','meðfram','mé','milli','nei','nema','né','niður','nú','og','ó','samkvæmt','sem','stundum','til','um','undan','undir','upp','uss','ú','úr','út','vegna','við','víða','yfir','ýmist','þar','þarna','þá','þegar','æ','ætíð','m.a','t.d','o.fl',' '];
 
-print "\n------- Byrja -------\n";
-
-# Open a file
+print "";
+print "- Les inn skrá (words.txt).";
 allWordsFile = open("words.txt", "r")
 # skrá splittað niður í orð
 allWordsRaw = [word for line in allWordsFile for word in line.split()];
@@ -21,30 +20,33 @@ allWordsRaw = [word for line in allWordsFile for word in line.split()];
 allWordsFile.close()
 
 
-#hreinsa drasl af orðunum
+print "- Hreinsa drasl af orðunum.";
 allWords = [];
 for rawWord in allWordsRaw:
-	strippedRawWord = rawWord.lower().strip(";:., \t\n\r»,«-");
+	strippedRawWord = rawWord.lower().strip('";:., \t\n\r»,«-()');
 	if(len(strippedRawWord)>1):
 		allWords.append(strippedRawWord);
 
 
-#Hendi út smáorðum
+print "- Hendi út smáorðum.";
 filteredWords = [];
 for aWord in allWords:
 	if not(aWord in badWords):
 		filteredWords.append(aWord);
 
-#print "filteredWords ---------------\n",filteredWords;
 
-# prenta fallega út
-print "\n\n";
 
+
+
+print "- Tel saman og skrifa í skrá (wordcount.txt).";
+
+outputFile = open("wordcount.txt", "wb")
 c = Counter(filteredWords);
 for word, count in c.most_common():
-	print word, "\t", count;
+	outputFile.write(word+"\t"+str(count)+"\n");
+outputFile.close()
 
-print "\n\n";
+print "- Búið!\n";
 
 
 
